1fd1fd43efd903db1dea43cb4a520b081b449c2e
DamianKaczmarczyk <jokernick@o2.pl>
2023-10-20 17:41:44 +0200

    comments-removed

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,18 +1,5 @@
 import styles from 'components/statistics/statistic.module.css';
 
-// const StaticticsList = ({ stats }) => {
-//   return (
-//     <ul className={styles.list}>
-//       {stats.map(stat => (
-//         <li className={styles.items}>
-//           <span>{stat.label}</span>
-//           <span>{stat.percentage}</span>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
 export default function Statistics({stats}) {
   return (
     <section className={styles.statistics}>
